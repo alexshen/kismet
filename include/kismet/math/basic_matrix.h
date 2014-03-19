731f3972062e76f6ee3a834a91e4d538ce729b73
Alex Shen <swlsww@hotmail.com>
2014-03-19 10:17:19 +0800

    add 1-d matrix specialization, more operators.

--- include/kismet/math/basic_matrix.h
+++ include/kismet/math/basic_matrix.h
@@ -3,8 +3,7 @@
 
 #include <cassert>
 #include <cstddef>
-#include <algorithm>
-#include <iterator>
+#include <numeric>
 #include "kismet/math/math_trait.h"
 #include "kismet/integer_sequence.h"
 #include "kismet/utility.h"
@@ -31,98 +30,133 @@ struct valid_dimension<N, Sizes...>
 };
 
 template<std::size_t N, std::size_t... Sizes>
-struct matrix_dim
+struct matrix_size
 {
     enum { value = N * matrix_dim<Sizes...>::value };
 };
 
 template<std::size_t N>
-struct matrix_dim<N>
+struct matrix_size<N>
 {
     enum { value = N };
 };
 
-template<typename T, typename Derived, std::size_t... Sizes>
+template<typename Derived, typename T, std::size_t... Sizes>
 class common_matrix_impl
 {
     static_assert(detail::valid_dimension<Sizes...>::value, "Dimension must not be 0");
 public:
     // element-wise addition
-    Derived& operator +=(Derived const& m);
-    // element-wise subtraction
-    Derived& operator -=(Derived const& m);
-    // scalar multiplication
-    Derived& operator *=(T k);
-    // scalar division
-    Derived& operator /=(T k);
+    Derived& operator +=(Derived const& m)
+    {
+        for (std::size_t i = 0; i < sizeof(m_a) / sizeof(m_a[0]); ++i) {
+            m_a[i] += m.m_a[i];
+        }
+        return static_cast<Derived&>(*this);
+    }
 
-    std::size_t dim(size_t index) const;
+    // element-wise subtraction
+    Derived& operator -=(Derived const& m)
+    {
+        for (std::size_t i = 0; i < sizeof(m_a) / sizeof(m_a[0]); ++i) {
+            m_a[i] -= m.m_a[i];
+        }
+        return static_cast<Derived&>(*this);
+    }
+
+    // scalar multiplication
+    Derived& operator *=(T k)
+    {
+        for (auto& e : m_a) {
+            e *= k;
+        }
+        return static_cast<Derived&>(*this);
+    }
+
+    // scalar division
+    Derived& operator /=(T k)
+    {
+        using std::abs;
+
+        assert(abs(k) > math_trait<T>::zero_tolerance());
+        *this *= inv(k);
+        return static_cast<Derived&>(*this);
+    }
+
+    // get the size of n-th dimension
+    std::size_t size(size_t index) const
+    {
+        assert(index < sizeof...(Sizes));
+        return get(index, utility::integer_sequence<std::size_t, Sizes...>());
+    }
+
+    // get the total number of elements
+    std::size_t size() const { return s_size; }
+
+    T* data() { return m_a;  }
+    T const* data() const { return m_a; }
 protected:
-    T m_a[detail::matrix_dim<Sizes...>::value];
+    static const std::size_t s_size = detail::matrix_size<Size...>::value;
+    T m_a[s_size];
 };
 
-template<typename T, typename Derived, std::size_t... Sizes>
-inline Derived& common_matrix_impl<T, Derived, Sizes...>::operator +=(Derived const& m)
-{
-    for (std::size_t i = 0; i < sizeof(m_a) / sizeof(m_a[0]); ++i) {
-        m_a[i] += m.m_a[i];
-    }
-    return static_cast<Derived&>(*this);
-}
-
-template<typename T, typename Derived, std::size_t... Sizes>
-inline Derived& common_matrix_impl<T, Derived, Sizes...>::operator -=(Derived const& m)
-{
-    for (std::size_t i = 0; i < sizeof(m_a) / sizeof(m_a[0]); ++i) {
-        m_a[i] -= m.m_a[i];
-    }
-    return static_cast<Derived&>(*this);
-}
-
-template<typename T, typename Derived, std::size_t... Sizes>
-inline Derived& common_matrix_impl<T, Derived, Sizes...>::operator *=(T k)
-{
-    for (auto& e : m_a) {
-        e *= k;
-    }
-    return static_cast<Derived&>(*this);
-}
-
-template<typename T, typename Derived, std::size_t... Sizes>
-inline Derived& common_matrix_impl<T, Derived, Sizes...>::operator /=(T k)
-{
-    using std::abs;
-
-    assert(abs(k) > math_trait<T>::zero_tolerance());
-    *this *= inv(k);
-    return static_cast<Derived&>(*this);
-}
-
-template<typename T, typename Derived, std::size_t... Sizes>
-inline std::size_t common_matrix_impl<T, Derived, Sizes...>::dim(std::size_t index) const
-{
-    assert(index < sizeof...(Sizes));
-    return get(index, utility::integer_sequence<std::size_t, Sizes...>());
-}
 } // namespace detail
 
 // fixed sized matrix with arbitrary number of dimsensions
 template<typename T, std::size_t... Sizes>
-class basic_matrix : public detail::common_matrix_impl<T, basic_matrix<T, Sizes...>, Sizes...>
+class basic_matrix : public detail::common_matrix_impl<basic_matrix<T, Sizes...>, T, Sizes...>
 {
 };
 
 template<typename T, std::size_t... Sizes>
-inline basic_matrix<T, Sizes...> operator *(T k, basic_matrix<T, Sizes...> const& m)
+inline basic_matrix<T, Sizes...> operator +(basic_matrix<T, Sizes...> m1, basic_matrix<T, Sizes...> const& m2)
 {
-    basic_matrix<T, Sizes...> tmp{ m };
-    tmp *= k;
-    return tmp;
+    return m1 += m2;
+}
+
+template<typename T, std::size_t... Sizes>
+inline basic_matrix<T, Sizes...> operator -(basic_matrix<T, Sizes...> m1, basic_matrix<T, Sizes...> const& m2)
+{
+    return m1 -= m2;
+}
+
+template<typename T, std::size_t... Sizes>
+inline basic_matrix<T, Sizes...> operator *(T k, basic_matrix<T, Sizes...> m)
+{
+    m *= k;
+    return m;
+}
+
+template<typename T, std::size_t... Sizes>
+inline basic_matrix<T, Sizes...> operator *(basic_matrix<T, Sizes...> m, T k)
+{
+    m *= k;
+    return m;
+}
+
+template<typename T, std::size_t... Sizes>
+inline basic_matrix<T, Sizes...> operator /(basic_matrix<T, Sizes...> m, T k)
+{
+    m /= k;
+    return m;
 }
 
 // avoid instantiating 0-d matrix
 template<typename T>
 class basic_matrix<T>;
+
+// 1-d matrix
+template<typename T, std::size_t N>
+class basic_matrix<T, N> : public detail::common_matrix_impl<basic_matrix<T, N>, T, N>
+{
+    static_assert(detail::valid_dimension<N>::value, "Dimension must not be 0");
+};
+
+template<typename T, std::size_t N>
+inline T operator *(basic_matrix<T, N> const& m1, basic_matrix<T, N> const& m2)
+{
+    return std::inner_product(m1.data(), m1.data() + m1.size(), m2.data(), T{});
+}
 
 // 2-d matrix
 template<typename T, std::size_t N1, std::size_t N2>
