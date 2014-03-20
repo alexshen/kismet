c9fee8de6088a2cce811e27d3342bce8acd33f3b
Alex Shen <swlsww@hotmail.com>
2014-03-20 14:35:07 +0800

    add initializer list constructor and interator support

--- include/kismet/math/matrix.h
+++ include/kismet/math/matrix.h
@@ -1,8 +1,10 @@
 #ifndef KISMET_MATH_BASIC_MATRIX_H
 #define KISMET_MATH_BASIC_MATRIX_H
 
+#include <algorithm>
 #include <cassert>
 #include <cstddef>
+#include <initializer_list>
 #include <numeric>
 #include "kismet/math/math_trait.h"
 #include "kismet/integer_sequence.h"
@@ -41,12 +43,64 @@ struct matrix_size<N>
     enum { value = N };
 };
 
+// Utility for making a nesting initializer list
+template<typename T, std::size_t N>
+struct make_matrix_initializer
+{
+    using type = std::initializer_list<typename make_matrix_initializer<T, N - 1>::type>;
+};
+
+template<typename T>
+struct make_matrix_initializer<T, 1>
+{
+    using type = std::initializer_list<T>;
+};
+
+template<typename T, std::size_t N>
+using matrix_initializer = typename make_matrix_initializer<T, N>::type;
+
+template<typename T, typename U, std::size_t N>
+T* insert_flat(T* data, std::initializer_list<U> const& l)
+{
+    for (auto& e : l) {
+        data = insert_flat(data, e);
+    }
+    return data;
+}
+
+template<typename T>
+T* insert_flat(T* data, std::initializer_list<T> const& l)
+{
+    std::copy(l.begin(), l.end(), data);
+    return data + l.size();
+}
+
 template<typename Derived, typename T, std::size_t... Sizes>
 class common_matrix_impl
 {
     static_assert(detail::valid_dimension<Sizes...>::value, "Dimension must not be 0");
 public:
-    using size_type = std::size_t;
+    using size_type              = std::size_t;
+    using difference_type        = std::ptrdiff_t;
+    using value_type             = T;
+    using reference_type         = T&;
+    using iterator               = T*;
+    using const_iterator         = T const*;
+    using reverse_iterator       = std::reverse_iterator<iterator>;
+    using const_reverse_iterator = std::reverse_iterator<const_iterator>;
+
+    // number of dimension of the matrix
+    static const size_type rank = sizeof...(Sizes);
+    // number of elements
+    static const size_type num  = detail::matrix_size<Sizes...>::value;
+
+    // default constructor, do nothing
+    common_matrix_impl() {}
+    
+    common_matrix_impl(matrix_initializer<T, rank> const& mi)
+    {
+        insert_flat(data(), mi);
+    }
 
     // element-wise addition
     Derived& operator +=(Derived const& m)
@@ -91,13 +145,29 @@ public:
     }
 
     // get the total number of elements
-    size_type size() const { return s_size; }
+    size_type size() const { return num; }
 
-    T* data() { return m_a;  }
+    T*       data() { return m_a;  }
     T const* data() const { return m_a; }
+
+    iterator       begin() { return iterator{ m_a }; }
+    iterator       end() { return iterator{ m_a + size() }; }
+    const_iterator begin() const { return m_a; }
+    const_iterator end() const { return m_a + size(); }
+
+    const_iterator cbegin() const { return begin(); }
+    const_iterator cend() const { return end();  }
+
+    reverse_iterator rbegin() { return reverse_iterator{ end() }; }
+    reverse_iterator rend() { return reverse_iterator{ begin() }; }
+
+    const_reverse_iterator rbegin() const { return const_reverse_iterator{ end() }; }
+    const_reverse_iterator rend() const { return const_reverse_iterator{ begin() }; }
+
+    const_reverse_iterator crbegin() const { return rbegin(); }
+    const_reverse_iterator crend() const { return crend(); }
 protected:
-    static const size_type s_size = detail::matrix_size<Size...>::value;
-    T m_a[s_size];
+    T m_a[num];
 };
 
 } // namespace detail
@@ -106,6 +176,14 @@ protected:
 template<typename T, std::size_t... Sizes>
 class basic_matrix : public detail::common_matrix_impl<basic_matrix<T, Sizes...>, T, Sizes...>
 {
+    using base_type = detail::common_matrix_impl<basic_matrix<T, Sizes...>, T, Sizes...>;
+public:
+    basic_matrix() {}
+
+    basic_matrix(detail::matrix_initializer<T, base_type::rank> const& mi)
+        : base_type{ mi }
+    {
+    }
 };
 
 template<typename T, std::size_t... Sizes>
@@ -141,31 +219,27 @@ inline basic_matrix<T, Sizes...> operator /(basic_matrix<T, Sizes...> m, T k)
     return m;
 }
 
+template<typename T, std::size_t... Sizes>
+inline bool operator ==(basic_matrix<T, Sizes...> const& m1, basic_matrix<T, Sizes...> const& m2)
+{
+    return std::equal(m1.begin(), m1.end(), m2.begin());
+}
+
+template<typename T, std::size_t... Sizes>
+inline bool operator !=(basic_matrix<T, Sizes...> const& m1, basic_matrix<T, Sizes...> const& m2)
+{
+    return !(m1 == m2);
+}
+
 // avoid instantiating 0-d matrix
 template<typename T>
 class basic_matrix<T>;
 
-// 1-d matrix
-template<typename T, std::size_t N>
-class basic_matrix<T, N> : public detail::common_matrix_impl<basic_matrix<T, N>, T, N>
-{
-    static_assert(detail::valid_dimension<N>::value, "Dimension must not be 0");
-};
-
 template<typename T, std::size_t N>
 inline T operator *(basic_matrix<T, N> const& m1, basic_matrix<T, N> const& m2)
 {
-    return std::inner_product(m1.data(), m1.data() + m1.size(), m2.data(), T{});
+    return std::inner_product(m1.begin(), m1.end(), m2.begin(), T{});
 }
-
-// 2-d matrix
-template<typename T, std::size_t N1, std::size_t N2>
-class basic_matrix<T, N1, N2>
-{
-    static_assert(detail::valid_dimension<N1, N2>::value, "Dimension must not be 0");
-private:
-    T m_a[N1][N2];
-};
 
 KISMET_CLASS_TEMPLATE_API(basic_matrix, float, 2)
 KISMET_CLASS_TEMPLATE_API(basic_matrix, float, 3)
