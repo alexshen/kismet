d889465521b4918531c4ee93f69ed9486eb879d2
Alex Shen <swlsww@hotmail.com>
2014-03-22 20:34:25 +0800

    move test main to main.cpp

--- source/test/linear_system_test.cpp
+++ source/test/linear_system_test.cpp
@@ -1,6 +1,5 @@
-#define BOOST_TEST_MODULE linear_system
 #include <algorithm>
-#include <boost/test/included/unit_test.hpp>
+#include <boost/test/unit_test.hpp>
 #include "kismet/math/linear_system.h"
 using namespace std;
 using namespace kismet::math;
---
+++ source/test/main.cpp
@@ -0,0 +1,2 @@
+#define BOOST_TEST_MODULE kismet
+#include <boost/test/included/unit_test.hpp>
