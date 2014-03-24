8538a7e904afad1eeae990247a11058b660c3cf2
Alex Shen <swlsww@hotmail.com>
2014-03-24 22:01:59 +0800

    rewrite test cases

--- source/test/linear_system_test.cpp
+++ source/test/linear_system_test.cpp
@@ -4,72 +4,47 @@
 using namespace std;
 using namespace kismet::math;
 
-BOOST_AUTO_TEST_CASE(solve_2x2)
+BOOST_AUTO_TEST_CASE(linear_system_solve2x2_determinant_zero_fail)
 {
-    {
-        // initialize an zero array
-        float a[2][2] = {};
-        float b[2] = {};
-        float c[2];
-        BOOST_CHECK(!solve(a, b, c));
-    }
-
-    {
-        // linear dependent row
-        float a[2][2] = {
-            { 1, 0 },
-            { 2, 0 }
-        };
-        float b[2] = {};
-        float c[2];
-        BOOST_CHECK(!solve(a, b, c));
-    }
-
-    {
-        // identity matrix
-        float a[2][2] = {
-            { 1, 0 },
-            { 0, 1 }
-        };
-        float b[2] = { 1, 1 };
-        float c[2];
-        BOOST_CHECK(solve(a, b, c));
-        BOOST_CHECK(equal(b, b+2, c));
-    }
+    // initialize an zero array
+    float a[2][2] = {};
+    float b[2] = {};
+    float c[2];
+    BOOST_CHECK(!solve(a, b, c));
 }
 
-BOOST_AUTO_TEST_CASE(solve_3x3)
+BOOST_AUTO_TEST_CASE(linear_system_solve2x2_identity_all_one)
 {
-    {
-        // initialize an zero array
-        float a[3][3] = {};
-        float b[3] = {};
-        float c[3];
-        BOOST_CHECK(!solve(a, b, c));
-    }
+    // identity matrix
+    float a[2][2] = {
+        { 1, 0 },
+        { 0, 1 }
+    };
+    float b[2] = { 1, 1 };
+    float c[2];
+    BOOST_CHECK(solve(a, b, c));
+    BOOST_CHECK(equal(b, b + 2, c));
+}
 
-    {
-        // linear dependent row
-        float a[3][3] = {
-            { 1, 0, 0 },
-            { 2, 0, 0 },
-            { 0, 0, 0 },
-        };
-        float b[3] = {};
-        float c[3];
-        BOOST_CHECK(!solve(a, b, c));
-    }
+BOOST_AUTO_TEST_CASE(linear_system_solve3x3_determinant_zero_fail)
+{
+    // initialize an zero array
+    float a[3][3] = {};
+    float b[3] = {};
+    float c[3];
+    BOOST_CHECK(!solve(a, b, c));
+}
 
-    {
-        // identity matrix
-        float a[3][3] = {
-            { 1, 0, 0 },
-            { 0, 1, 0 },
-            { 0, 0, 1 }
-        };
-        float b[3] = { 1, 1, 1 };
-        float c[3];
-        BOOST_CHECK(solve(a, b, c));
-        BOOST_CHECK(equal(b, b+3, c));
-    }
+BOOST_AUTO_TEST_CASE(linear_system_solve3x3_identity_all_one)
+{
+    // identity matrix
+    float a[3][3] = {
+        { 1, 0, 0 },
+        { 0, 1, 0 },
+        { 0, 0, 1 }
+    };
+    float b[3] = { 1, 1, 1 };
+    float c[3];
+    BOOST_CHECK(solve(a, b, c));
+    BOOST_CHECK(equal(b, b + 3, c));
 }
--- source/test/matrix_test.cpp
+++ source/test/matrix_test.cpp
@@ -2,47 +2,22 @@
 #include "kismet/math/matrix.h"
 using namespace kismet::math;
 
-BOOST_AUTO_TEST_CASE(matrix_init)
+BOOST_AUTO_TEST_CASE(matrix_1d_rank_1)
 {
-    matrix2f mf2{1, 2};
-    BOOST_CHECK_EQUAL(mf2.size(), 2);
     BOOST_CHECK_EQUAL(matrix2f::rank, 1);
+}
+
+BOOST_AUTO_TEST_CASE(matrix2f_num_2)
+{
     BOOST_CHECK_EQUAL(matrix2f::num, 2);
+}
 
-    matrix3f mf3{1, 2, 3};
-    BOOST_CHECK_EQUAL(mf3.size(), 3);
-    BOOST_CHECK_EQUAL(matrix3f::rank, 1);
-    BOOST_CHECK_EQUAL(matrix3f::num, 3);
-
-    matrix4f mf4{1, 2, 3, 4};
-    BOOST_CHECK_EQUAL(mf4.size(), 4);
-    BOOST_CHECK_EQUAL(matrix4f::rank, 1);
-    BOOST_CHECK_EQUAL(matrix4f::num, 4);
-
-    matrix22f mf22{
-        {1, 2},
-        {3, 4}
-    };
-    BOOST_CHECK_EQUAL(mf22.size(), 4);
+BOOST_AUTO_TEST_CASE(matrix_2d_rank_2)
+{
     BOOST_CHECK_EQUAL(matrix22f::rank, 2);
+}
+
+BOOST_AUTO_TEST_CASE(matrix22f_num_4)
+{
     BOOST_CHECK_EQUAL(matrix22f::num, 4);
-
-    matrix33f mf33{
-        {1, 2, 3},
-        {4, 5, 6},
-        {7, 8, 9}
-    };
-    BOOST_CHECK_EQUAL(mf33.size(), 9);
-    BOOST_CHECK_EQUAL(matrix33f::rank, 2);
-    BOOST_CHECK_EQUAL(matrix33f::num, 9);
-
-    matrix44f mf44{
-        {1, 2, 3, 4},
-        {5, 6, 7, 8},
-        {9, 10, 11, 12},
-        {13, 14, 15, 16},
-    };
-    BOOST_CHECK_EQUAL(mf44.size(), 16);
-    BOOST_CHECK_EQUAL(matrix44f::rank, 2);
-    BOOST_CHECK_EQUAL(matrix44f::num, 16);
 }
