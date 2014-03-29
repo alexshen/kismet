240b56e519b1cf21bae11905c471ec2037248569
Alex Shen <swlsww@hotmail.com>
2014-03-29 13:34:45 +0800

    add module header

---
+++ include/kismet/core.h
@@ -0,0 +1,7 @@
+#ifndef KISMET_CORE_H
+#define KISMET_CORE_H
+
+#include "kismet/core/assert.h"
+
+#endif // KISMET_CORE_H
+
---
+++ include/kismet/math.h
@@ -0,0 +1,10 @@
+#ifndef KISMET_MATH_H
+#define KISMET_MATH_H
+
+#include "kismet/math/linear_system.h"
+#include "kismet/math/math.h"
+#include "kismet/math/math_trait.h"
+#include "kismet/math/utility.h"
+
+#endif // KISMET_MATH_H
+
