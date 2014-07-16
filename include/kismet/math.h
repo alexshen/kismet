5046dba379233c3225d6d7a008befff5eb2e2d47
Alex Shen <swlsww@hotmail.com>
2014-07-16 22:01:30 +0800

    add matrix, vector headers to math.h

--- include/kismet/math.h
+++ include/kismet/math.h
@@ -3,7 +3,9 @@
 
 #include "kismet/math/linear_system.h"
 #include "kismet/math/math_trait.h"
+#include "kismet/math/matrix.h"
 #include "kismet/math/utility.h"
+#include "kismet/math/vector.h"
 
 #endif // KISMET_MATH_H
 
