13ce2e089ed853ae3b4323c3dad8cb203e284c67
Alex Shen <swlsww@hotmail.com>
2014-07-17 22:08:17 +0800

    fix test case of is_comparable

--- source/test/utility_test.cpp
+++ source/test/utility_test.cpp
@@ -6,6 +6,6 @@ BOOST_AUTO_TEST_CASE(is_comparable_test)
 {
     struct not_comparable {};
     
-    BOOST_CHECK((is_comparable<not_comparable, not_comparable>::value));
+    BOOST_CHECK((!is_comparable<not_comparable, not_comparable>::value));
     BOOST_CHECK((is_comparable<int, int>::value));
 }
