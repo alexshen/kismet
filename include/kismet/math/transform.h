586de449389939b5a313609c932cecbbc5230070
Alex Shen <swlsww@hotmail.com>
2016-09-25 21:07:05 +0800

    explicit conversion for constants

--- include/kismet/math/transform.h
+++ include/kismet/math/transform.h
@@ -27,23 +27,23 @@ matrix44<T> quat_to_matrix(quaternion<T> const& q)
     T yz = q.y() * q.z();
     T wx = q.w() * q.x();
 
-    res[0][0] = 2 * (w2 + q.x() * q.x()) - 1;
-    res[0][1] = 2 * (xy - wz);
-    res[0][2] = 2 * (xz + wy);
-    res[0][3] = 0;
+    res[0][0] = T(2) * (w2 + q.x() * q.x()) - T(1);
+    res[0][1] = T(2) * (xy - wz);
+    res[0][2] = T(2) * (xz + wy);
+    res[0][3] = T(0);
 
-    res[1][0] = 2 * (xy + wz);
-    res[1][1] = 2 * (w2 + q.y() * q.y()) - 1;
-    res[1][2] = 2 * (yz - wx);
-    res[1][3] = 0;
+    res[1][0] = T(2) * (xy + wz);
+    res[1][1] = T(2) * (w2 + q.y() * q.y()) - T(1);
+    res[1][2] = T(2) * (yz - wx);
+    res[1][3] = T(0);
 
-    res[2][0] = 2 * (xz - wy);
-    res[2][1] = 2 * (yz + wx);
-    res[2][2] = 2 * (w2 + q.z() * q.z()) - 1;
-    res[2][3] = 0;
+    res[2][0] = T(2) * (xz - wy);
+    res[2][1] = T(2) * (yz + wx);
+    res[2][2] = T(2) * (w2 + q.z() * q.z()) - T(1);
+    res[2][3] = T(0);
 
-    res[3][0] = res[3][1] = res[3][2] = 0;
-    res[3][3] = 1;
+    res[3][0] = res[3][1] = res[3][2] = T(0);
+    res[3][3] = T(1);
 
     return res;
 }
@@ -54,9 +54,9 @@ quaternion<T> matrix_to_quat(matrix44<T> const& m)
     quaternion<T> res;
     // trace = 4w^2 - 1
     T trace = m[0][0] + m[1][1] + m[2][2];
-    if (trace > 0)
+    if (trace > T(0))
     {
-        res.w(std::sqrt(trace + 1) * T(0.5));
+        res.w(std::sqrt(trace + T(1)) * T(0.5));
         T inv_denom = T(0.25) / res.w();
         res.x(inv_denom * (m[2][1] - m[1][2]));
         res.y(inv_denom * (m[0][2] - m[2][0]));
@@ -67,7 +67,7 @@ quaternion<T> matrix_to_quat(matrix44<T> const& m)
         int i = max_index(m[0][0], m[1][1], m[2][2]);
         if (i == 0)
         {
-            res.x(std::sqrt(1 + m[0][0] - m[1][1] - m[2][2]) * T(0.5));
+            res.x(std::sqrt(T(1) + m[0][0] - m[1][1] - m[2][2]) * T(0.5));
             T inv_denom = T(0.25) / res.x();
             res.y(inv_denom * (m[0][1] + m[1][0]));
             res.z(inv_denom * (m[0][2] + m[2][0]));
@@ -75,7 +75,7 @@ quaternion<T> matrix_to_quat(matrix44<T> const& m)
         }
         else if (i == 1)
         {
-            res.y(std::sqrt(1 - m[0][0] + m[1][1] - m[2][2]) * T(0.5));
+            res.y(std::sqrt(T(1) - m[0][0] + m[1][1] - m[2][2]) * T(0.5));
             T inv_denom = T(0.25) / res.y();
             res.x(inv_denom * (m[0][1] + m[1][0]));
             res.z(inv_denom * (m[1][2] + m[2][1]));
@@ -83,7 +83,7 @@ quaternion<T> matrix_to_quat(matrix44<T> const& m)
         }
         else
         {
-            res.z(std::sqrt(1 - m[0][0] - m[1][1] + m[2][2]) * T(0.5));
+            res.z(std::sqrt(T(1) - m[0][0] - m[1][1] + m[2][2]) * T(0.5));
             T inv_denom = T(0.25) * res.z();
             res.x(inv_denom * (m[0][2] + m[2][0]));
             res.y(inv_denom * (m[1][2] + m[2][1]));
