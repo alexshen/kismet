ee0d39ad09149927c3fb519b8286b5ae44dcd5c9
Alex Shen <swlsww@hotmail.com>
2017-09-24 09:16:45 +0800

    migrate to CMake build system

--- clang_make.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-premake4 gmake && python change_gmake_compiler.py --cc=clang --cxx=clang++ build
--- source/test/main.cpp
+++ source/test/main.cpp
@@ -5,4 +5,5 @@ extern "C" void putenv(char*);
 #endif
 
 #define BOOST_TEST_MODULE kismet
-#include <boost/test/included/unit_test.hpp>
+#define BOOST_TEST_DYN_LINK
+#include <boost/test/unit_test.hpp>
